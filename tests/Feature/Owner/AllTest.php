f152674c670d43cbd385b2befa560463e082c372
Andrey Helldar <helldar@dragon-code.pro>
2026-07-09 01:40:21 +0300

    Added tests for settings owners

---
+++ tests/Feature/Owner/AllTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Workbench\App\Models\User;
+use Workbench\Database\Factories\UserFactory;
+
+test('success', function () {
+    $user1 = UserFactory::new()->create();
+    $user2 = UserFactory::new()->create();
+    $user3 = UserFactory::new()->create();
+
+    (new User)->defaultSettings()->set('foo', 111);
+    (new User)->defaultSettings()->set('bar', 222);
+
+    $user1->settings()->set('foo', 333);
+    $user2->settings()->set('bar', 444);
+
+    $result1 = $user1->settings()->all()->sortKeys()->all();
+    $result2 = $user2->settings()->all()->sortKeys()->all();
+    $result3 = $user3->settings()->all()->sortKeys()->all();
+
+    expect($result1)->toBe(['bar' => 222, 'foo' => 333]);
+    expect($result2)->toBe(['bar' => 444, 'foo' => 111]);
+    expect($result3)->toBe(['bar' => 222, 'foo' => 111]);
+});
