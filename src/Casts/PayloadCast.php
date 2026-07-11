4b7c0dc7ee546bcbb0197b0d39314e94ea8571d0
Andrey Helldar <helldar@dragon-code.pro>
2026-07-11 23:49:44 +0300

    Worked with model casting

--- src/Casts/PayloadCast.php
+++ src/Casts/PayloadCast.php
@@ -5,23 +5,15 @@ declare(strict_types=1);
 namespace DragonCode\LaravelModelSettings\Casts;
 
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
-use Illuminate\Contracts\Support\Arrayable;
-use Illuminate\Contracts\Support\Jsonable;
 use Illuminate\Database\Eloquent\Casts\Json;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Date;
 use Spatie\LaravelData\Data;
 
 use function blank;
 use function class_exists;
 use function config;
 use function is_a;
-use function json_decode;
-use function json_encode;
-use function json_validate;
 
 class PayloadCast implements CastsAttributes
 {
@@ -34,66 +26,52 @@ class PayloadCast implements CastsAttributes
         }
 
         if (! $cast = $this->cast($model)) {
-            return $this->decode($value);
+            return $this->fromJson($value);
+        }
+
+        if (is_a($cast, CastsAttributes::class, true)) {
+            return (new $cast())->get($model, $key, $value, $attributes);
         }
 
         if (class_exists(Data::class) && is_a($cast, Data::class, true)) {
             return $cast::from($value);
         }
 
-        if (class_exists($cast)) {
-            return new $cast(...$this->decode($value));
-        }
-
-        return match ($cast) {
-            'int',
-            'integer'                   => (int) $value,
-            'string'                    => (string) $value,
-            'bool',
-            'boolean'                   => (bool) $value,
-            'object'                    => $this->fromJson($value, true),
-            'collection'                => new Collection($this->fromJson($value)),
-            'date'                      => $this->fromDate($value),
-            'datetime',
-            'custom_datetime'           => $this->fromDateTime($value),
-            'immutable_date'            => $this->fromDate($value)->toImmutable(),
-            'immutable_datetime',
-            'immutable_custom_datetime' => $this->fromDateTime($value)->toImmutable(),
-            'timestamp'                 => $this->fromTimestamp($value),
-            default                     => $this->fromJson($value)
-        };
+        return $this->fromJson($value);
     }
 
     /**
      * @throws \JsonException
      */
-    public function set(Model $model, string $key, mixed $value, array $attributes): string
+    public function set(Model $model, string $key, mixed $value, array $attributes): ?string
     {
-        return match (true) {
-            $value instanceof Jsonable  => $value->toJson($this->flags),
-            $value instanceof Arrayable => $this->encode($value->toArray()),
-            default                     => $this->encode($value),
-        };
+        if (blank($value)) {
+            return null;
+        }
+
+        if (! $cast = $this->cast($model)) {
+            return $this->asJson($value);
+        }
+
+        if (class_exists(Data::class) && $value instanceof Data) {
+            return $value->toJson($this->flags);
+        }
+
+        if (is_a($cast, CastsAttributes::class, true)) {
+            return (new $cast())->set($model, $key, $value, $attributes);
+        }
+
+        return $this->asJson($value);
     }
 
-    protected function fromJson($value, $asObject = false): array|int|string|float|bool|null
+    protected function fromJson($value): array|string|int|float|bool|null
     {
-        return Json::decode($value, ! $asObject);
+        return Json::decode($value, true);
     }
 
-    protected function fromDate(string $value): ?Carbon
+    protected function asJson($value): string
     {
-        return Carbon::parse($value)->startOfDay();
-    }
-
-    protected function fromDateTime(string $value): ?Carbon
-    {
-        return Date::parse($value);
-    }
-
-    protected function fromTimestamp(string $value): int
-    {
-        return $this->fromDateTime($value)->timestamp;
+        return Json::encode($value, $this->flags);
     }
 
     protected function cast(Model $model): ?string
@@ -105,28 +83,13 @@ class PayloadCast implements CastsAttributes
 
     protected function parentModel(Model $model): string
     {
-        return Relation::getMorphedModel($model->item_type) ?? $model->item_type;
+        $type = $model->getAttribute('item_type');
+
+        return Relation::getMorphedModel($type) ?? $type;
     }
 
     protected function casts(): array
     {
         return config()->array('model_settings.casts', []);
-    }
-
-    /**
-     * @throws \JsonException
-     */
-    protected function encode(mixed $value): string
-    {
-        return json_encode($value, $this->flags);
-    }
-
-    protected function decode(string $value): mixed
-    {
-        if (! json_validate($value)) {
-            return null;
-        }
-
-        return json_decode($value, true);
     }
 }
